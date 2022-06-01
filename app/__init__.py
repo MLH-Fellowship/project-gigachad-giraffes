ab96455bf822dd4e8f7d562d75ffc5b5affcb438
rysonw <rysonplays@gmail.com>
2022-06-01 07:44:16 -1000

    About Section with Card Template

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 
 @app.route('/')
