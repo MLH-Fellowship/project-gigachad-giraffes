86487d2520c7f7de785ac0a77422f8cd9c3e2c71
Tanzebruh <tanzirhasanhasan@gmail.com>
2022-06-01 16:58:05 -0700

    created a sample map that uses Mappa leaflets to highlight places a person has been using data put in a simple csv. Json data for places is input in another file.

---
+++ app/static/scripts/sketch.js
@@ -0,0 +1,49 @@
+let visited;
+let countries;
+const mappa = new Mappa('Leaflet');
+let visitmap;
+let canvas;
+
+let data = [];
+
+const options = {
+  lat: 0,
+  lng: 0,
+  zoom: 1.5,
+  style: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+};
+
+function preload() {
+  visited = loadTable('../static/data/visited.csv', 'header');
+  
+  countries = loadJSON('../static/data/places.json');
+}
+
+function setup() {
+  canvas = createCanvas(800, 600);
+  visitmap = mappa.tileMap(options);
+  visitmap.overlay(canvas);
+
+  for (let row of visited.rows) {
+    let coords = countries[row.get('id')];
+    let latitude = coords[0]
+    let longitude = coords[1]
+    data.push({latitude,longitude});
+  }
+
+  console.log(data);
+
+  // console.log(countries);
+  //console.log(youtubeData);
+}
+
+function draw() {
+  clear();
+  for (let country of data) {
+    const pix = visitmap.latLngToPixel(country.latitude, country.longitude);
+    fill(255, 0, 200, 100);
+    const zoom = visitmap.zoom();
+    const scl = pow(2, zoom); // * sin(frameCount * 0.1);
+    ellipse(pix.x, pix.y, 3 * scl);
+  }
+}
