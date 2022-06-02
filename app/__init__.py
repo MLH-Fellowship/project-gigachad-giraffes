eedd6eb5dab7ee99fee97cbd34f5fe29ba331231
Tanzebruh <tanzirhasanhasan@gmail.com>
2022-06-02 08:58:05 -0700

    The initial templating is finished, will look into templating loops and the map

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__, static_folder='static')
 
-
+userinfo = {'name': 'Alan Turing',
+    'shortIntro': 'Aspiring Skynet Developer and Professional Programmer',
+    'longIntro': 'Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite"'
+ }
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), name="Alan Turing")
+    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), name=userinfo['name'],
+    shortIntro=userinfo['shortIntro'], longIntro = userinfo['longIntro'])
