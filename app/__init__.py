a4adbf05bc1d91f7632c5f1d0e0515ed76735243
Tanzebruh <tanzirhasanhasan@gmail.com>
2022-06-02 10:28:19 -0700

    Added templating for most functions, cleaned up unnecessary text' '

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,14 @@ app = Flask(__name__, static_folder='static')
 
 userinfo = {'name': 'Alan Turing',
     'shortIntro': 'Aspiring Skynet Developer and Professional Programmer',
-    'longIntro': 'Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite"'
+    'longIntro': 'Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite"',
+    'work': [{'jobTitle': 'Software Engineer @ MLH', 'desc': "I created the backend of the LMS", "year": "1999"},
+        {'jobTitle': 'Software Engineer @ Meta', 'desc': "I created facebook mobile application", "year": "2004-2022"}],
+    'skills': 'Python, Flask, Googling, JavaScript, Unity',
+    'education': [{'type': 'PhD of Computer Science', 'from': 'Columbia University', 'when': '1984-1989'}]
  }
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"), name=userinfo['name'],
-    shortIntro=userinfo['shortIntro'], longIntro = userinfo['longIntro'])
+    return render_template('index.html', title='MLH Fellow', url=os.getenv('URL'), name=userinfo['name'],
+    shortIntro=userinfo['shortIntro'], longIntro = userinfo['longIntro'], work = userinfo['work'], skills = userinfo['skills'],
+    education = userinfo['education'])
