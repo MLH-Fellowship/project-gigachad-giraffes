2ef2048a217b13ea913f7c139b5f0af5a47ed666
rysonw <rysonplays@gmail.com>
2022-06-03 05:04:51 -1000

    Added footer and hobbies page

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,26 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__, static_folder='static')
 
+userinfo = {'name': 'Alan Turing',
+    'shortIntro': 'Aspiring Skynet Developer and Professional Programmer',
+    'longIntro': 'Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite"Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite"',
+    'work': [{'jobTitle': 'Software Engineer @ MLH', 'desc': "I created the backend of the LMS", "year": "1999"},
+        {'jobTitle': 'Software Engineer @ Meta', 'desc': "I created facebook mobile application", "year": "2004-2022"}],
+    'skills': 'Python, Flask, Googling, JavaScript, Unity',
+    'education': [{'type': 'PhD of Computer Science', 'from': 'Columbia University', 'when': '1984-1989'}],
+    'email': '123fakemail@gmail.com',
+    'hobbies':  [{'title': 'Basketball', 'desc': 'My Favorite Sport!'}, {'title': 'Fishing', 'desc': 'My favorite way to relax!'}, {'title': 'Paddleboarding', 'desc': 'My favorite watersport!'}]
+ }
 
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', 
+    title="Hobbies", 
+    url=os.getenv("URL"),
+    hobbies = userinfo["hobbies"]
+    )
+
