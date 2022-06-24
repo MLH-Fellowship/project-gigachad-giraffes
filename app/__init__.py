640575f51948c9130ff01deb6efe0dee57cd3f1d
Tanzebruh <tanzirhasanhasan@gmail.com>
2022-06-24 11:16:14 -0700

    Made changes to improve timeline site and removed redundant css

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, render_template, request, flash
+from flask_gravatar import Gravatar
 from dotenv import load_dotenv
 from playhouse.shortcuts import model_to_dict
 from peewee import *
@@ -30,9 +31,24 @@ class TimelinePost(Model):
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
-userinfo = {'name': 'Alan Turing',
-    'shortIntro': 'Aspiring Skynet Developer and Professional Programmer',
-    'longIntro': 'Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite". Alan Turing is from Kansas City, Missouri. He obtained his PhD at Yale. He currently works as a Software Developer. He is a driven individual who plans to one day work as a for his dream company: "Epic Games", and work on his favorite game, "Fortnite"',
+gravatar = Gravatar(app,
+                    size=900,
+                    rating='g',
+                    default='retro',
+                    force_default=False,
+                    force_lower=False,
+                    use_ssl=False,
+                    base_url=None)
+
+
+userinfo = {'name': 'Tanzir Hasan',
+    'shortIntro': 'Aspiring Software Developer and Cryptography Enthusiast',
+    'longIntro': ['''I\'m Tanzir! I\'m currently going to NYU and getting my degree in Computer Science with a minor in Mathematics. I like learning about how math is applied to computer science and I\'m particularly interested in computer graphics and physics simulations.
+    I have worked in multiple aspects of software engineering from backend systems to mobile applications to implementing algorithms.''', 
+    '''\nI program a lot on my own and I really enjoy it, I'm currently working on a audio equalizer in C++. My favorite programming languages are Python and C++, but Haskell and Kotlin are a close second.
+     I am currently an MLH Fellow and interning at Boeing. My biggest professional goal is to create a self contained homomorphic system''',
+    '''\n\nAside from programming, I have a crippling addiction to books, particularly Fantasy. I go through books very quickly and I'm currently reading a 
+    book called The Poppy War, you should check it out! Aside from that I read books on programming from time to time, I'm currently reading through Automota and Computability by Dexter C. Kozen on a recommendation from my math addicted friend.'''],
     'work': [{'jobTitle': 'Software Engineer @ MLH', 'desc': "I created the backend of the LMS", "year": "1999", 'link':'./static/img/logo.jpg'},
         {'jobTitle': 'Software Engineer @ Meta', 'desc': "I created facebook mobile application", "year": "2004-2022", 'link':'./static/img/logo.jpg'}],
     'skills': ['./static/img/skillicons/c-.png','./static/img/skillicons/css-3.png', './static/img/skillicons/html-5.png',
@@ -69,7 +85,7 @@ def openweathermap(city_name):
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='MLH Fellow', url=os.getenv('URL'), name=userinfo['name'],
+    return render_template('index.html', title='Tanzir Hasan', url=os.getenv('URL'), name=userinfo['name'],
     shortIntro=userinfo['shortIntro'], longIntro = userinfo['longIntro'], work = userinfo['work'], skills = userinfo['skills'],
     education = userinfo['education'], email=userinfo['email'], facebook = userinfo['facebook'], instagram = userinfo['instagram'],
     github=userinfo['github'], linkedin=userinfo['linkedin'], twitter = userinfo['twitter'], profilepic='./static/img/profile.jpg',
@@ -109,6 +125,8 @@ def post_time_line_post():
     email = request.form['email']
     content = request.form['content']
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    if timeline_post.id > 10:
+        TimelinePost.delete_by_id(timeline_post.id-10)
     return redirect(request.referrer)
 
 @app.route('/api/timeline_post', methods=['GET'])
@@ -118,15 +136,15 @@ def get_time_line_post():
         'timeline_posts': [
             model_to_dict(p)
             for p in 
-            TimelinePost.select().order_by(TimelinePost.created_at.desc())
+            TimelinePost.select().where(TimelinePost.id > 0).order_by(TimelinePost.created_at.desc())
         ]
     }
 
 @app.route('/api/timeline_post', methods=['DELETE'])
 
 def delete_time_line_post():
-    mydb.drop_tables([TimelinePost])#Deletes all posts made for demo
-    mydb.create_tables([TimelinePost])
+    for i in range(int(request.form['start']), int(request.form['end'])+1):
+        TimelinePost.delete_by_id(i)
     return 'Emptied'
 
 @app.route('/timeline')
