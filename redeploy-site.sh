c91dca1ddfa3d17e30514c4d4b2e019dfc3e8626
rysonw <rysonplays@gmail.com>
2022-06-24 05:14:08 -1000

    Changed redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,5 @@
 cd flask-portfolio
-
 git fetch && git reset origin/main --hard
-
 source python3-virtualenv/bin/activate
-
 python3 pip install -r requirements.txt
-
 tmux new-session -d -s flask-portfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
