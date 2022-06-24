94ae9deeb6d3af68aa4fbf5b03a709d3442e1576
Tanzebruh <tanzirhasanhasan@gmail.com>
2022-06-24 11:50:47 -0700

    made changes to redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
-echo existing servers killed
+systemctl daemon-reload
 
 cd ~/project-gigachad-giraffes
 
 git fetch && git reset origin/main --hard
 
-source python3-virtualenv/bin/activate
-
-pip install -r requirements.txt
-
-tmux new-session -d -s myportfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myportfolio
