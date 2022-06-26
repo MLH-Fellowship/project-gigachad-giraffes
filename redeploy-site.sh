bea65380963038270d57a1f671c9453ffa8621ca
Ryson Wong <91065673+rysonw@users.noreply.github.com>
2022-06-26 09:32:53 -1000

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
-tmux kill-server
-cd flask-portfolio
+#!/bin/bash
+systemctl daemon-reload
+cd ~/flask-portfolio
 git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-tmux new-session -d -s flask-portfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+systemctl restart myportfolio
