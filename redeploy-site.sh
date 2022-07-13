87e1c9bca4d3d7abe9d2f364726897f297c8ffa4
rysonw <rysonplays@gmail.com>
2022-07-12 19:58:38 -1000

    changes

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 cd ~/flask-portfolio
-
 git fetch && git reset origin/main --hard
-
 docker compose -f docker-compose.prod.yml down
-
 docker compose -f docker-compose.prod.yml up -d --build
