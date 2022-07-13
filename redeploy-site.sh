7eae0821dd8573fe775047317d6a28da70f20532
rysonw <rysonplays@gmail.com>
2022-07-12 16:13:27 -1000

    script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,6 +2,6 @@
 systemctl daemon-reload
 cd flask-portfolio
 git fetch && git reset origin/main --hard
-source env1/bin/activate
+source env_main/bin/activate
 pip install -r requirements.txt
 systemctl restart myportfolio
