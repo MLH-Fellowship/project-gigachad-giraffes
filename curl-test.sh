b4b9234d46b674ca7d0eb8a8ee37115d02a10bee
Ryson Wong <91065673+rysonw@users.noreply.github.com>
2022-07-16 22:05:00 -1000

    Delete curl-test.sh

--- curl-test.sh
+++
@@ -1,5 +0,0 @@
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Ryson&email=fake@gmail.com&content=This is a Message'
-
-curl --request GET http://localhost:5000/api/timeline_post
-
-curl --request DELETE http://localhost:5000/api/timeline_post
